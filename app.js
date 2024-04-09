d0625920f2c014c2e4d3ec6738e3aea6cd1bafbe
unknown <josh-pye@live.nmit.ac.nz>
2024-04-09 13:48:23 +1200

    creating a grid with checkAccess function

---
+++ app.js
@@ -0,0 +1,35 @@
+/*
+Create a function that takes a grid of $ and - where each ($) represents a mine and each (-) represents mine-free spot
+Return an array where each ($) is replaced by a digit indicating the number of mines immediately mined adjacent to the spot
+
+mineGrid([
+    ["-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-"],
+    ["-", "-", "$", "-", "-"],
+    ["-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-"]
+])
+
+output -> [
+   ["0", "0", "0", "0", "0"],
+    ["0", "1", "1", "1", "0"],
+    ["0", "1", "$", "1", "0"],
+    ["0", "1", "1", "1", "0"],
+    ["0", "0", "0", "0", "0"]
+]) 
+]
+*/
+//Create a function that accepts a 2D array arr
+const mineGrid = (arr) => {
+    //Create a helper function that access the element position (y,x) in arr. if the y index is valid or exists 
+    //I want to return arr[y][x]; otherwise return null (This will prevent accessing undefined elements and to avoid errors
+    const checkAccess = (y,x) => arr[y] ? arr[y][x] : null
+    //Create another helper function that calculates the number of adjacent mines around the ell at position (y,x)
+    const getMine = (x,y) => {
+        //an array is costructed with elements that represents the content of adjacent cells
+        //we are going to use checkAccess for each of the eight possible adjacent positions
+        return [
+
+        ]
+    }
+}
